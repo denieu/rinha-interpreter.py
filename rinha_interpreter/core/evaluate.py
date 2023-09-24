ea46e1142ba8b9dc2e4bd7b92942eaee1ca243e9
Daniel Wojcickoski <dswojcickoski@gmail.com>
2023-09-24 18:53:46 -0300

    enhancement: More efficient SpecTerm eval

--- rinha_interpreter/core/evaluate.py
+++ rinha_interpreter/core/evaluate.py
@@ -20,8 +20,7 @@ from rinha_interpreter.core.spec import (
     SpecVar,
 )
 
-
-binary_operators: dict[SpecBinaryOp, Callable[[SpecEvaluateReturn, SpecEvaluateReturn], bool]] = {
+spec_binary_ops: dict[SpecBinaryOp, Callable[[SpecEvaluateReturn, SpecEvaluateReturn], bool]] = {
     SpecBinaryOp.Add: lambda _lhs, _rhs: f"{_lhs}{_rhs}" if isinstance(_lhs, str) or isinstance(_rhs, str) else _lhs + _rhs,
     SpecBinaryOp.Sub: lambda _lhs, _rhs: _lhs - _rhs,
     SpecBinaryOp.Mul: lambda _lhs, _rhs: _lhs * _rhs,
@@ -38,101 +37,125 @@ binary_operators: dict[SpecBinaryOp, Callable[[SpecEvaluateReturn, SpecEvaluateR
 }
 
 
-def evaluate_binary_operator(operator: SpecBinaryOp, lhs: SpecEvaluateReturn, rhs: SpecEvaluateReturn) -> bool:
-    return binary_operators[operator](lhs, rhs)
+def _eval_spec_int(_term: SpecInt, _environment: Environment) -> int:
+    return int(_term.value)
+
+
+def _eval_spec_str(_term: SpecStr, _environment: Environment) -> str:
+    return int(_term.value)
+
+
+def _eval_spec_call(_term: SpecCall, _environment: Environment) -> SpecEvaluateReturn:
+    spec_call_callee = evaluate(_term.callee, _environment)
+    if not isinstance(spec_call_callee, SpecFunction):
+        raise Exception("Invalid callable")
+
+    new_scope = {}
+    for index, parameter in enumerate(spec_call_callee.parameters):
+        argument = _term.arguments[index]
+
+        parameter_name = parameter.text
+        parameter_value = evaluate(argument, _environment)
+
+        new_scope[parameter_name] = parameter_value
+
+    _environment.start_scope(new_scope)
+    result = evaluate(spec_call_callee.value, _environment)
+    _environment.finish_scope()
+
+    return result
+
+
+def _eval_spec_binary(_term: SpecBinary, _environment: Environment) -> bool:
+    return spec_binary_ops[_term.op](evaluate(_term.lhs, _environment), evaluate(_term.rhs, _environment))
+
+
+def _eval_spec_function(_term: SpecFunction, _environment: Environment) -> SpecFunction:
+    return _term
+
+
+def _eval_spec_let(_term: SpecLet, _environment: Environment) -> SpecEvaluateReturn:
+    _environment.set_variable(_term.name.text, evaluate(_term.value, _environment))
+    return evaluate(_term.next, _environment)
+
+
+def _eval_spec_if(_term: SpecIf, _environment: Environment) -> int:
+    spec_if_condition_result = evaluate(_term.condition, _environment)
+
+    if spec_if_condition_result:
+        return evaluate(_term.then, _environment)
+
+    return evaluate(_term.otherwise, _environment)
+
+
+def _eval_spec_print(_term: SpecPrint, _environment: Environment) -> int:
+    spec_print_result = evaluate(_term.value, _environment)
+
+    if isinstance(spec_print_result, str):
+        print(spec_print_result)
+
+    elif isinstance(spec_print_result, (int, float)):
+        print(spec_print_result)
+
+    elif isinstance(spec_print_result, bool):
+        print(str(spec_print_result).lower())
+
+    elif isinstance(spec_print_result, tuple):
+        print(spec_print_result)
+
+    elif isinstance(spec_print_result, SpecFunction):
+        print("<#closure>")
+
+    else:
+        raise Exception("Tipo invalido no print")
+
+    return spec_print_result
+
+
+def _eval_spec_first(_term: SpecFirst, _environment: Environment) -> int:
+    spec_first_result = evaluate(_term.value, _environment)
+    if not isinstance(spec_first_result, tuple):
+        raise Exception("Esperava que isso fosse uma tupla")
+
+    return spec_first_result[0]
+
+
+def _eval_spec_second(_term: SpecSecond, _environment: Environment) -> int:
+    spec_second_result = evaluate(_term.value, _environment)
+    if not isinstance(spec_second_result, tuple):
+        raise Exception("Esperava que isso fosse uma tupla")
+
+    return spec_second_result[1]
+
+
+def _eval_spec_bool(_term: SpecBool, _environment: Environment) -> int:
+    return bool(_term.value)
+
+
+def _eval_spec_tuple(_term: SpecTuple, _environment: Environment) -> int:
+    return evaluate(_term.first, _environment), evaluate(_term.second, _environment)
+
+
+def _eval_spec_var(_term: SpecVar, _environment: Environment) -> int:
+    return _environment.get_variable(_term.text)
+
+
+spec_terms: dict[SpecTerm, Callable[[SpecTerm, Environment], SpecEvaluateReturn]] = {
+    SpecInt: _eval_spec_int,
+    SpecStr: _eval_spec_str,
+    SpecCall: _eval_spec_call,
+    SpecBinary: _eval_spec_binary,
+    SpecFunction: _eval_spec_function,
+    SpecLet: _eval_spec_let,
+    SpecIf: _eval_spec_if,
+    SpecPrint: _eval_spec_print,
+    SpecFirst: _eval_spec_first,
+    SpecSecond: _eval_spec_second,
+    SpecBool: _eval_spec_bool,
+    SpecTuple: _eval_spec_tuple,
+    SpecVar: _eval_spec_var,
+}
 
 
 def evaluate(term: SpecTerm, environment: Environment) -> SpecEvaluateReturn:
-    term_type = type(term)
-
-    if term_type == SpecInt:
-        return int(term.value)
-
-    if term_type == SpecStr:
-        return str(term.value)
-
-    if term_type == SpecCall:
-        spec_call_callee = evaluate(term.callee, environment)
-        if not isinstance(spec_call_callee, SpecFunction):
-            raise Exception("Invalid callable")
-
-        new_scope = {}
-        for index, parameter in enumerate(spec_call_callee.parameters):
-            argument = term.arguments[index]
-
-            parameter_name = parameter.text
-            parameter_value = evaluate(argument, environment)
-
-            new_scope[parameter_name] = parameter_value
-
-        environment.start_scope(new_scope)
-        result = evaluate(spec_call_callee.value, environment)
-        environment.finish_scope()
-
-        return result
-
-    if term_type == SpecBinary:
-        return evaluate_binary_operator(term.op, evaluate(term.lhs, environment), evaluate(term.rhs, environment))
-
-    if term_type == SpecFunction:
-        return term
-
-    if term_type == SpecLet:
-        environment.set_variable(term.name.text, evaluate(term.value, environment))
-        return evaluate(term.next, environment)
-
-    if term_type == SpecIf:
-        spec_if_condition_result = evaluate(term.condition, environment)
-
-        if spec_if_condition_result:
-            return evaluate(term.then, environment)
-
-        return evaluate(term.otherwise, environment)
-
-    if term_type == SpecPrint:
-        spec_print_result = evaluate(term.value, environment)
-
-        if isinstance(spec_print_result, str):
-            print(spec_print_result)
-
-        elif isinstance(spec_print_result, (int, float)):
-            print(spec_print_result)
-
-        elif isinstance(spec_print_result, bool):
-            print(str(spec_print_result).lower())
-
-        elif isinstance(spec_print_result, tuple):
-            print(spec_print_result)
-
-        elif isinstance(spec_print_result, SpecFunction):
-            print("<#closure>")
-
-        else:
-            raise Exception("Tipo invalido no print")
-
-        return spec_print_result
-
-    if term_type == SpecFirst:
-        spec_first_result = evaluate(term.value, environment)
-        if not isinstance(spec_first_result, tuple):
-            raise Exception("Esperava que isso fosse uma tupla")
-
-        return spec_first_result[0]
-
-    if term_type == SpecSecond:
-        spec_second_result = evaluate(term.value, environment)
-        if not isinstance(spec_second_result, tuple):
-            raise Exception("Esperava que isso fosse uma tupla")
-
-        return spec_second_result[1]
-
-    if term_type == SpecBool:
-        return bool(term.value)
-
-    if term_type == SpecTuple:
-        return evaluate(term.first, environment), evaluate(term.second, environment)
-
-    if term_type == SpecVar:
-        return environment.get_variable(term.text)
-
-    raise Exception("Term invalido")
+    return spec_terms[type(term)](term, environment)
